2f7f2a714925e99d7deb754ff7373fedea1c3202
Raul Pacheco <61039190+raulpacheco2k@users.noreply.github.com>
2022-07-03 19:48:04 -0300

    Update configure_ubuntu_work_environment.sh

--- configure_ubuntu_work_environment.sh
+++ configure_ubuntu_work_environment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
   
   apt_programs=(
+    curl
     git
     snap
   )
@@ -83,14 +84,14 @@
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
   echo "Configuring Docker to work without sudo permission..."
   sudo addgroup --system docker
