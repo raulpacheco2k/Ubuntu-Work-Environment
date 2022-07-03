a3004ec8523dc6a743f694f3bc112d3854009378
Raul Pacheco <61039190+raulpacheco2k@users.noreply.github.com>
2022-07-03 18:09:24 -0300

    Update linux_programs.sh

--- linux_programs.sh
+++ linux_programs.sh
@@ -69,7 +69,7 @@
 
   echo "Installing Google Chrome..."
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -i ./google-chrome-stable_current_amd64.deb
+  sudo dpkg -i ./google-chrome-stable_current_amd64.deb
   rm google-chrome-stable_current_amd64.deb
   
   echo "Installing Anydesk..."
