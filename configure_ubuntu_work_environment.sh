1fa93567c921bb62aa66852eea78bab8dbef579b
raulpacheco2k <eu@raulpacheco.com.br>
2025-04-27 23:30:10 -0300

    Adicione configuração para chave SSH

--- configure_ubuntu_work_environment.sh
+++ configure_ubuntu_work_environment.sh
@@ -2,21 +2,30 @@
 
   echo "Updating repositories..."
   sudo apt update -y
-  
+
   echo "Customizing Ubuntu..."
   gsettings set org.gnome.desktop.interface enable-animations true
   gsettings set org.gnome.nautilus.preferences default-sort-order 'type'
-  
+
+  echo "Generating SSH keys..."
+  ssh-keygen -t ed25519 -C "eu@raulpacheco.com.br"
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_ed25519
+
   echo "Configuring Git..."
   git config --global credential.helper store
   git config --global user.email eu@raulpacheco.com.br
   git config --global user.name raulpacheco2k
-  
+  git config --global gpg.format ssh
+  git config --global user.signingKey "$(cat ~/.ssh/id_ed25519.pub)"
+  git config --global commit.gpgsign true
+  git config --global tag.gpgsign true
+
   echo "Installing Google Chrome..."
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   sudo dpkg -i ./google-chrome-stable_current_amd64.deb
   rm google-chrome-stable_current_amd64.deb
-  
+
   apt_programs=(
     curl
     neofetch
@@ -55,19 +64,19 @@
     "spotify"
     "gedit"
   )
-  
+
   echo "Installing APT packages..."
   for apt_program in "${apt_programs[@]}"; do
     sudo apt install "$apt_program" -y
   done
-  
+
   echo "Installing SNAP packages..."
   for snap_program in "${snap_programs[@]}"; do
     sudo snap install "$snap_program"
   done
-  
+
   snap connect nordpass:password-manager-service
-  
+
   sudo snap install phpstorm --classic
   sudo snap install pycharm-professional --classic
   sudo snap install intellij-idea-ultimate --classic
@@ -87,11 +96,11 @@
   sudo snap install sublime-merge --classic
   sudo snap install sublime-text --classic
   sudo snap install obsidian --classic
-  
+
   echo "Installing AI..."
   curl -fsSL https://ollama.com/install.sh | sh
   ollama run gemma3:1b
-  
+
   echo "Finalizing, updating and cleaning "
   sudo apt update -y
   sudo apt --fix-broken install
@@ -106,10 +115,10 @@
   sudo bleachbit --clean system.cache system.trash system.tmp
   sudo fstrim -av
   sudo updatedb
-  
+
   read -p "Setup completed. Do you want to restart your computer now? [Y/N]: " option
   if [ "$option" == "y" ] || [ "$option" == "Y" ]; then
     sudo reboot
   fi
-  
+
   exit 0
